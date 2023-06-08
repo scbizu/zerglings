15c47359da3f1359447cfc3d1d6ed98c7bdd4399
scnace <scbizu@gmail.com>
2023-06-08 16:39:01 +0800

    complete first 11

--- exercises/005_arrays2.zig
+++ exercises/005_arrays2.zig
@@ -22,15 +22,9 @@ pub fn main() void {
     const le = [_]u8{ 1, 3 };
     const et = [_]u8{ 3, 7 };
 
-    // (Problem 1)
-    // Please set this array concatenating the two arrays above.
-    // It should result in: 1 3 3 7
-    const leet = ???;
+    const leet = le ++ et;
 
-    // (Problem 2)
-    // Please set this array using repetition.
-    // It should result in: 1 0 0 1 1 0 0 1 1 0 0 1
-    const bit_pattern = [_]u8{ ??? } ** 3;
+    const bit_pattern = [_]u8{ 1, 0, 0, 1 } ** 3;
 
     // Okay, that's all of the problems. Let's see the results.
     //
